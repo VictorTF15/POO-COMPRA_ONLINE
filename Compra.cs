594330d923e73f5d74dbe23d8124279ecf196da6
VictorTF15 <utp0210623@alumno.utpuebla.edu.mx>
2025-05-19 11:28:39 -0600

    Add files via upload

---
+++ Compra.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompraOnline
+{
+  public class Compra
+  {
+    public Cliente Cliente { get; set; }
+    public double Total { get; set; }
+    public bool PagoExitoso { get; set; }
+
+    public Compra(Cliente cliente, double total)
+    {
+      Cliente = cliente;
+      Total = total;
+    }
+
+    public string ProcesarPago() 
+    {
+      Random random = new Random();
+      PagoExitoso = random.Next(0, 2) == 1;
+
+
+      if (PagoExitoso)
+        return $"Compra realizada por {Cliente.Nombre}. Total: {Total}";
+      else
+        return $"Error en el pago. Intente con otro método.";
+    }
+
+
+
+  }
+}
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompraOnline
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      string nombre = textBox1.Text;
+      Cliente cliente1 = new Cliente(nombre);
+      double total = double.Parse(textBox2.Text);
+      Compra compra1 = new Compra(cliente1, total);
+
+      label3.Text = compra1.ProcesarPago();
+
+    }
+
+    private void label3_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
